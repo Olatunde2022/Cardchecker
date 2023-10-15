0b9ec3dd79e1e39d392be795d54703433f5c26f9
unknown <ojekab2580@gmail.com>
2023-10-15 12:39:37 -0700

    another commit

--- Card_Base/urls.py
+++ Card_Base/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -23,5 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('myCards.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
