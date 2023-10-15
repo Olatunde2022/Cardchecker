c14424cbf9e665fc66be9e73a7a7699f5ef92694
unknown <ojekab2580@gmail.com>
2023-10-15 12:00:02 -0700

    another commit

--- Card_Base/settings.py
+++ Card_Base/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECRET_KEY = 'django-insecure-m%figuk!5qpkuc32)n=zr0(^xis+61lf-8ieb)o6e_rd)@x1fk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
